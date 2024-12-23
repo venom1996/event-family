5ac8939873608d2ae58c9c52514fab04a96f258b
Евгений <venom1996@inbox.ru>
2024-12-23 18:45:57 +0300

    add telegram bot

--- app/Services/Post/AffairsService.php
+++ app/Services/Post/AffairsService.php
@@ -5,9 +5,20 @@ namespace App\Services\Post;
 use App\Models\AffairsFamily;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
-
+use TelegramBot\Api\BotApi;
 class AffairsService
 {
+
+    private BotApi $telegramApi;
+    private string $chatId;
+
+    public function __construct()
+    {
+        $this->telegramApi = new BotApi(env('TELEGRAM_BOT_TOKEN'));
+        $this->chatId = env('TELEGRAM_CHAT_ID');
+    }
+
+
     public function add(Request $request)
     {
         $currentDateTime = date('Y-m-d H:i:s');
@@ -28,7 +39,21 @@ class AffairsService
 
         $save = AffairsFamily::create($arAdd);
 
+        $userName = Auth::getUser()['name'];
+
         if ($save->id > 0) {
+            $utf8emoji = "\u{1F4A9}\u{1F4A9}\u{1F4A9}";
+            $message = 'Добавлено новое дело! ' . $utf8emoji .  "\n" .
+                'ID: ' . $save->id . "\n" .
+                'Кто добавил: ' . $userName . "\n" .
+                'Дата начала: ' . $currentDateTime . "\n" .
+                'Дата просрочки: ' . $dateFinish . "\n" .
+                'Название: ' . $save->name . "\n" .
+                'Описание:' . "\n" .
+                $save->description;
+
+            $this->telegramApi->sendMessage($this->chatId, $message);
+
             return $save->id;
         }
     }
@@ -41,16 +66,22 @@ class AffairsService
     public function editModal(Request $request) :void
     {
         $currentUsersId = Auth::id();
+        $userName = Auth::getUser()['name'];
         if ($request->assign) {
+            $emodji = "\u{1F49A}\u{1F49A}";
+            $message = $userName . 'взял на себя обязанность выполнить дело c ID' . $request->id . $emodji;
             $arUpdate = [
                 'user_id_working' => $currentUsersId,
                 'status' => 'assign'
             ];
         } else if ($request->finish) {
+            $emodji = "\u{1F92A}\u{1F92A}";
+            $message = $userName . ' выполнил дело с ID ' . $request->id . $emodji;
             $arUpdate = [
                 'status' => 'finish'
             ];
         } else {
+            $message = $userName . ' отредактировал дело с ID ' . $request->id;
             $arUpdate = [
                 'name' => $request->name,
                 'description' => $request->description,
@@ -60,5 +91,7 @@ class AffairsService
         AffairsFamily::where('id', $request->id)->update(
             $arUpdate
         );
+
+        $this->telegramApi->sendMessage($this->chatId, $message);
     }
 }
